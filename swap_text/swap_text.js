d094ba984e99b4054b8d9551b18933dd37167294
Rodrigo Candido Faria <8796054+rfaria@users.noreply.github.com>
2022-05-19 19:55:12 -0300

    Delete swap_text directory

--- swap_text/swap_text.js
+++
@@ -1,8 +0,0 @@
-function swap_text() {
-  var x = document.getElementById("myDIV");
-  if (x.innerHTML === "Hello") {
-    x.innerHTML = "Swapped text!";
-  } else {
-    x.innerHTML = "Hello";
-  }
-}
