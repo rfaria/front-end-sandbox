6788eaff4db896db6fadabb383a10a8096939cae
Rodrigo <8796054+rfaria@users.noreply.github.com>
2023-02-11 21:24:10 -0300

    Update test_karel_stanford.js

--- test_karel_stanford.js
+++ test_karel_stanford.js
@@ -1,10 +1,11 @@
 /**
+ * https://stanford.edu/~cpiech/karel/ide.html
  * Welcome to the Stanford Karel IDE.
  * This is a free space for you to 
  * write any Karel program you want.
  **/
 function main(){
-   putBeeperAlongDiagonal();
+   createChessPattern();
 }
 
 function goToExtremeDiagonal(){
@@ -24,5 +25,43 @@ function putBeeperAlongDiagonal(){
       move();
       putBeeper();
       turnRight();
+   }
+}
+
+function createOddRowChessPattern(){
+   while(frontIsClear()){
+      putBeeper();
+      move();
+      move();
+   }
+   putBeeper();
+}
+
+function createEvenRowChessPattern(){
+   while(frontIsClear()){
+      move();
+      putBeeper();
+      move();
+   }
+}
+
+function goToUpperRowFromRightWall(){
+   turnLeft();
+   move();
+   turnLeft();
+}
+
+function goToUpperRowFromLeftWall(){
+   turnRight();
+   move();
+   turnRight();
+}
+
+function createChessPattern(){
+   while(leftIsClear() || rightIsClear()){
+      createOddRowChessPattern();
+      goToUpperRowFromRightWall();
+      createEvenRowChessPattern();
+      goToUpperRowFromLeftWall();
    }
 }
